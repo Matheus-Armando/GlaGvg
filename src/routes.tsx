eb3daef63a66d0087006a95098992df6f0e2afa6
Matheus-Armando <141080917+Matheus-Armando@users.noreply.github.com>
2024-09-16 01:17:09 -0300

    feat(LoginPage): add login page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import IslandReview from './pages/IslandReview';
+import LoginPage from './pages/LoginPage';
 
 const AppRoutes: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<IslandReview />} />
+        <Route path="/login" element={<LoginPage />} />
         {/* Adicione outras rotas conforme necessário */}
       </Routes>
     </Router>
