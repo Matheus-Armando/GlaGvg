9780ae82d9c71a03e72c384900912dad97820dd9
Matheus-Armando <141080917+Matheus-Armando@users.noreply.github.com>
2024-09-16 01:24:08 -0300

    fix(LoginPage): visual stuff

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,3 +1,4 @@
+import Layout from '@/components/Layout/Layout';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
@@ -5,31 +6,31 @@ import React from 'react';
 
 
 const LoginPage: React.FC = () => {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
-      <Card className="w-full max-w-md p-4">
-        <CardHeader className="text-center">
-          <h2 className="text-2xl font-bold">Login</h2>
-        </CardHeader>
-        <CardContent>
-          <form>
-            <div className="mb-4">
-              <label className="block text-gray-700 dark:text-gray-300">Username</label>
-              <Input type="email" placeholder="Enter your username" className="w-full mt-2" />
-            </div>
-            <div className="mb-4">
-              <label className="block text-gray-700 dark:text-gray-300">Password</label>
-              <Input type="password" placeholder="Enter your password" className="w-full mt-2" />
-            </div>
-            <Button type="submit" className="w-full mt-4">Login</Button>
-          </form>
-        </CardContent>
-        <CardFooter className="text-center">
-          <p className="text-gray-700 dark:text-gray-300">Don't have an account? <a href="/register" className="text-blue-500">Register</a></p>
-        </CardFooter>
-      </Card>
-    </div>
-  );
+    return (
+        <Layout>    
+                <Card className="w-full max-w-md p-4">
+                    <CardHeader className="text-center">
+                        <h2 className="text-2xl font-bold">Login</h2>
+                    </CardHeader>
+                    <CardContent>
+                        <form>
+                            <div className="mb-4">
+                                <label className="block text-gray-700 dark:text-gray-500">Username</label>
+                                <Input type="email" placeholder="Enter your username" className="w-full mt-2" />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block text-gray-700 dark:text-gray-500">Password</label>
+                                <Input type="password" placeholder="Enter your password" className="w-full mt-2" />
+                            </div>
+                            <Button type="submit" className="w-full mt-4">Login</Button>
+                        </form>
+                    </CardContent>
+                    <CardFooter className="text-center">
+                        <p className="text-gray-700 dark:text-gray-300">Don't have an account? <a href="/register" className="text-blue-500">Register</a></p>
+                    </CardFooter>
+                </Card>       
+        </Layout>
+    );
 };
 
 export default LoginPage;
