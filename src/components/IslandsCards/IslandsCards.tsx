11983c5a747681a7d28b7fd7869a99f7f2b6d157
Matheus-Armando <141080917+Matheus-Armando@users.noreply.github.com>
2024-09-18 01:02:55 -0300

    fix(confia)

--- src/components/IslandsCards/IslandsCards.tsx
+++ src/components/IslandsCards/IslandsCards.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Card, CardContent, CardFooter, CardHeader } from '../ui/card';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '../ui/tooltip';
 
-
 interface IslandCardProps {
   id: number;
   title: string;
@@ -14,24 +13,26 @@ interface IslandCardProps {
 const IslandCard: React.FC<IslandCardProps> = ({ title, image, recImage, onClick }) => {
   return (
     <Card className="cursor-pointer" onClick={onClick}>
-      <CardHeader className="text-center">
-        <h2 className="text-xl font-bold">{title}</h2>
-      </CardHeader>
-      <CardContent className="flex justify-center">
-        <img src={image} alt={title} className="w-full h-auto" />
-      </CardContent>
-      <CardFooter className="flex justify-center text-center">
-        <TooltipProvider>
-        <Tooltip>
-            <TooltipTrigger>
-                <button className="text-gray-900">Recompensas</button>
-            </TooltipTrigger>
-            <TooltipContent>
-              <img src={recImage} alt={`Recompensas ${title}`} className="w-96 h-auto" />
-            </TooltipContent>
+      <div className="bg-white dark:bg-gray-800 rounded-lg shadow-md p-4">
+        <CardHeader className="text-center">
+          <h2 className="text-xl font-bold text-gray-900 dark:text-gray-100">{title}</h2>
+        </CardHeader>
+        <CardContent className="flex justify-center">
+          <img src={image} alt={title} className="w-full h-auto" />
+        </CardContent>
+        <CardFooter className="flex justify-center text-center">
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>
+                <button className="text-gray-900 dark:text-gray-100">Recompensas</button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <img src={recImage} alt={`Recompensas ${title}`} className="w-96 h-auto" />
+              </TooltipContent>
             </Tooltip>
-        </TooltipProvider>
-      </CardFooter>
+          </TooltipProvider>
+        </CardFooter>
+      </div>
     </Card>
   );
 };
