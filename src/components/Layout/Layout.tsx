d6b04807a0ef7b4f1eed836f3f707e1010e15e77
Matheus-Armando <141080917+Matheus-Armando@users.noreply.github.com>
2024-09-20 02:06:27 -0300

    feat(Breadcrumb): delete title and add initial prototype off breadcrumb

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,23 +1,36 @@
 import React from 'react';
 import Banner from '../banner';
-import { Card, CardHeader } from '../ui/card';
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from '../ui/breadcrumb';
 
 interface LayoutProps {
   children: React.ReactNode;
-  title?: string;
+  breadcrumbItems?: { label: string; href?: string }[];
 }
 
-const Layout: React.FC<LayoutProps> = ({ children, title }) => {
+const Layout: React.FC<LayoutProps> = ({ children, breadcrumbItems }) => {
+  const defaultBreadcrumbItems = [{ label: 'Home', href: '/' }, ...(breadcrumbItems || [])];
+
   return (
     <div className="min-h-screen flex flex-col bg-gray-100 dark:bg-gray-900">
       <Banner />
-      {title && (
-        <div className="flex justify-center items-center flex-grow mb-12">
-          <Card className="max-w-max">
-            <CardHeader className="bg-gray-200 dark:bg-gray-800 text-center py-4">
-              <h1 className="text-2xl font-bold text-gray-900 dark:text-gray-100">{title}</h1>
-            </CardHeader>
-          </Card>
+      {defaultBreadcrumbItems && (
+        <div className="flex justify-center items-center mb-12">
+          <Breadcrumb>
+            <BreadcrumbList>
+              {defaultBreadcrumbItems.map((item, index) => (
+                <React.Fragment key={index}>
+                  <BreadcrumbItem>
+                    {item.href ? (
+                      <BreadcrumbLink href={item.href}>{item.label}</BreadcrumbLink>
+                    ) : (
+                      <BreadcrumbPage>{item.label}</BreadcrumbPage>
+                    )}
+                  </BreadcrumbItem>
+                  {index < defaultBreadcrumbItems.length - 1 && <BreadcrumbSeparator />}
+                </React.Fragment>
+              ))}
+            </BreadcrumbList>
+          </Breadcrumb>
         </div>
       )}
       <main className="flex-grow flex flex-col items-center justify-center w-full">
--- src/pages/IslandPage.tsx
+++ src/pages/IslandPage.tsx
@@ -21,7 +21,7 @@ const IslandsPage: React.FC = () => {
   };
 
   return (
-    <Layout title={'Ilhas'}>
+    <Layout>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {islands.map((island) => (
           <IslandCard
